73026e59b2e9bbf7ebfa190d3650ecfb811818df
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-07-20 21:24:48 +0300

    notification as read

--- routers/notification.js
+++ routers/notification.js
@@ -13,4 +13,30 @@ router.get("/", async (req, res) => {
     console.log(error);
   }
 });
+
+// notification as read
+router.put("/:notificationId", async (req, res) => {
+  const { notificationId } = req.params;
+  const loggedInUserId = req.user.userId;
+
+  try {
+    const notification = await Notification.findOneAndUpdate(
+      { _id: notificationId, userId: loggedInUserId },
+      { isRead: true },
+      { new: true }
+    );
+
+    if (!notification) {
+      return res.status(404).json({ message: "Notification not found." });
+    }
+
+    res.status(200).json({ data: notification });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error marking notification as read.", error });
+    console.log(error);
+  }
+});
+
 module.exports = router;
