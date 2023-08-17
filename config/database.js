3e9af912f0be3639b0243fa0848438b0b90afab1
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-08-17 15:12:17 +0300

    updates

--- config/database.js
+++ config/database.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  if (!process.env.MONGO_URL) {
+    console.error("MONGO_URL environment variable is not set.");
+    process.exit(1);
+  }
+
   try {
     await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
